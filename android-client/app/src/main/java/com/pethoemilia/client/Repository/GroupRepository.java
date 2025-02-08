b048aa45924cec6df3073bbc19fc4ec4269c772c
pethoemilia <120325025+emilypetho@users.noreply.github.com>
2025-02-08 11:34:14 +0200

    group

--- android-client/app/src/main/java/com/pethoemilia/client/Repository/GroupRepository.java
+++ android-client/app/src/main/java/com/pethoemilia/client/Repository/GroupRepository.java
@@ -1,13 +1,11 @@
 package com.pethoemilia.client.Repository;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
-import com.google.gson.Gson;
+import com.pethoemilia.client.LoginActivity;
 import com.pethoemilia.client.MyConst;
 import com.pethoemilia.client.api.GroupClient;
 import com.pethoemilia.client.api.MessageClient;
@@ -16,86 +14,84 @@ import com.pethoemilia.client.entity.Message;
 import com.pethoemilia.client.entity.User;
 
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-//public class GroupRepository {
-//    private final GroupClient groupClient;
-//    private final MessageClient messageClient;
-//    private final SharedPreferences sharedPreferences;
-//    private final MutableLiveData<List<Group>> groupsLiveData = new MutableLiveData<>();
-//
-//    public GroupRepository(Context context) {
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl(MyConst.URL)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build();
-//
-//        groupClient = retrofit.create(GroupClient.class);
-//        messageClient = retrofit.create(MessageClient.class);
-//        sharedPreferences = context.getSharedPreferences(MyConst.SHARED_PREF_KEY, Context.MODE_PRIVATE);
-//    }
-//
-//    public LiveData<List<Group>> getGroupsLiveData() {
-//        return groupsLiveData;
-//    }
-//
-//    public void loadGroups(long userId) {
-//        String encodedCredentials = sharedPreferences.getString(MyConst.AUTH, null);
-//        Call<List<Group>> call = groupClient.findByUserId(userId, encodedCredentials);
-//
-//        call.enqueue(new Callback<List<Group>>() {
-//            @Override
-//            public void onResponse(Call<List<Group>> call, Response<List<Group>> response) {
-//                if (response.isSuccessful() && response.body() != null) {
-//                    groupsLiveData.setValue(response.body());
-//                } else {
-//                    Log.e("GroupRepository", "Hiba a csoportok betöltésekor: " + response.code());
-//                    if (response.code() == 401) {
-//                        // Jogosultsági hiba, lehet kezelni itt
-//                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Group>> call, Throwable t) {
-//                Log.e("GroupRepository", "Hálózati hiba: ", t);
-//            }
-//        });
-//    }
-//
-//    public void loadMessagesForGroup(Long groupId) {
-//        String encodedCredentials = sharedPreferences.getString(MyConst.AUTH, null);
-//        Call<List<Message>> call = messageClient.findByGroupId(groupId, encodedCredentials);
-//
-//        call.enqueue(new Callback<List<Message>>() {
-//            @Override
-//            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
-//                if (response.isSuccessful() && response.body() != null) {
-//                    List<Message> messages = response.body();
-//                    List<Group> groups = groupsLiveData.getValue();
-//                    if (groups != null) {
-//                        for (Group group : groups) {
-//                            if (group.getId().equals(groupId)) {
-//                                group.setMessages(messages);
-//                                break;
-//                            }
-//                        }
-//                        groupsLiveData.postValue(groups);
-//                    }
-//                } else {
-//                    Log.e("GroupRepository", "Hiba az üzenetek betöltésekor: " + response.message());
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Message>> call, Throwable t) {
-//                Log.e("GroupRepository", "Hálózati hiba az üzenetek lekérésekor", t);
-//            }
-//        });
-//    }
-//}
+public class GroupRepository {
+    private final GroupClient groupClient;
+    private final MessageClient messageClient;
+
+    public interface GroupCallback {
+        void onGroupsLoaded(List<Group> groups);
+    }
+
+    public GroupRepository() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MyConst.URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        groupClient = retrofit.create(GroupClient.class);
+        messageClient = retrofit.create(MessageClient.class);
+    }
+
+    public void loadGroups(long userId, Context context, GroupCallback callback) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(MyConst.SHARED_PREF_KEY, Context.MODE_PRIVATE);
+        String encodedCredentials = sharedPreferences.getString(MyConst.AUTH, null);
+
+        Call<List<Group>> call = groupClient.findByUserId(userId, encodedCredentials);
+        call.enqueue(new Callback<List<Group>>() {
+            @Override
+            public void onResponse(Call<List<Group>> call, Response<List<Group>> response) {
+                if (response.isSuccessful()) {
+                    List<Group> groups = response.body();
+                    if (groups != null) {
+                        loadMessagesForGroups(groups, context, callback);
+                    } else {
+                        callback.onGroupsLoaded(null);
+                    }
+                } else {
+                    handleUnauthorized(context);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Group>> call, Throwable t) {
+                Log.e("GroupRepository", "Network error", t);
+            }
+        });
+    }
+
+    private void loadMessagesForGroups(List<Group> groups, Context context, GroupCallback callback) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(MyConst.SHARED_PREF_KEY, Context.MODE_PRIVATE);
+        String encodedCredentials = sharedPreferences.getString(MyConst.AUTH, null);
+
+        for (Group group : groups) {
+            Call<List<Message>> call = messageClient.findByGroupId(group.getId(), encodedCredentials);
+            call.enqueue(new Callback<List<Message>>() {
+                @Override
+                public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                    if (response.isSuccessful()) {
+                        group.setMessages(response.body());
+                    } else {
+                        handleUnauthorized(context);
+                    }
+                    callback.onGroupsLoaded(groups);
+                }
+
+                @Override
+                public void onFailure(Call<List<Message>> call, Throwable t) {
+                    Log.e("GroupRepository", "Network error", t);
+                }
+            });
+        }
+    }
+
+    private void handleUnauthorized(Context context) {
+        Intent intent = new Intent(context, LoginActivity.class);
+        context.startActivity(intent);
+    }
+}
--- android-client/app/src/main/java/com/pethoemilia/client/ViewModel/GroupViewModel.java
+++ android-client/app/src/main/java/com/pethoemilia/client/ViewModel/GroupViewModel.java
@@ -1,35 +1,53 @@
 package com.pethoemilia.client.ViewModel;
 
-import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
+import com.google.gson.Gson;
+import com.pethoemilia.client.MyConst;
+import com.pethoemilia.client.Repository.GroupRepository;
 import com.pethoemilia.client.entity.Group;
-//import com.pethoemilia.client.Repository.GroupRepository;
+import com.pethoemilia.client.entity.User;
 
 import java.util.List;
 
-//public class GroupViewModel extends AndroidViewModel {
-//    private final GroupRepository repository;
-//    private final LiveData<List<Group>> groupsLiveData;
-//
-//    public GroupViewModel(@NonNull Application application) {
-//        super(application);
-//        repository = new GroupRepository(application);
-//        groupsLiveData = repository.getGroupsLiveData();
-//    }
-//
-//    public LiveData<List<Group>> getGroupsLiveData() {
-//        return groupsLiveData;
-//    }
-//
-//    public void loadGroups(long userId) {
-//        repository.loadGroups(userId);
-//    }
-//
-//    public void loadMessagesForGroup(Long groupId) {
-//        repository.loadMessagesForGroup(groupId);
-//    }
-//}
+public class GroupViewModel extends ViewModel {
+    private final GroupRepository repository;
+    private final MutableLiveData<List<Group>> groupsLiveData = new MutableLiveData<>();
+
+    public GroupViewModel() {
+        repository = new GroupRepository();
+    }
+
+    public LiveData<List<Group>> getGroups() {
+        return groupsLiveData;
+    }
+
+    public void loadGroups(long userId, Context context) {
+        repository.loadGroups(userId, context, groups -> {
+            groupsLiveData.postValue(groups);
+        });
+    }
+
+    public User getUserFromSharedPreferences(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(MyConst.SHARED_PREF_KEY, Context.MODE_PRIVATE);
+        String userJson = sharedPreferences.getString(MyConst.USER, null);
+        if (userJson != null) {
+            return new Gson().fromJson(userJson, User.class);
+        }
+        return null;
+    }
+
+    public void saveGroupToSharedPreferences(Context context, Group group) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(MyConst.SHARED_PREF_KEY, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(MyConst.GROUP, new Gson().toJson(group));
+        editor.apply();
+    }
+}
